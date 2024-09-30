d3c6494083bf8ec1cc1ca6e24eb80dc79b349c21
LeafYeeXYZ <leafyeexyz@outlook.com>
2024-09-30 16:42:07 +0800

    fix: style

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,7 +1,7 @@
 export default function Introduction() {
 
   return (
-    <div className='w-full max-w-xl text-balance text-center flex flex-col items-center justify-center gap-4 mt-5 mb-24 px-2'>
+    <div className='w-full max-w-xl text-balance text-center flex flex-col items-center justify-center gap-2 mt-5 mb-24 px-2'>
       
       <p className='text-yellow-800'>
         「所有诗句均由支教中的孩子们创作」
@@ -9,10 +9,10 @@ export default function Introduction() {
       <p className='text-lg mt-16 mb-2'>
         北京师范大学白鸽青协“夏日启行”项目组负责筹备、组织并管理白鸽青协暑期支教团。通过学校招募与调研、支教工作考评、课程设计与培训、统筹管理与宣传为各支教队提供前期保障与发展规划，致力于为远方的孩子带去一个更加充实的夏天
       </p>
-      <p className='text-yellow-900 leading-3'>
+      <p className='text-yellow-900'>
         「我们于秋日播种，覆冬雪，迎春阳，等待夏日一场共同的盛放」
       </p>
-      <p className='text-yellow-900 leading-3'>
+      <p className='text-yellow-900'>
         「白鸽衔梦，草木贲华」我们共同期待着一场无拘无束、尽致淋漓的成长
       </p>
     </div>
